baaf4e54a1dfee63a3ae22c1c33735549107a634
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-02-11 10:14:11 +0530

    Add ownership verification to update and delete post controllers

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -54,11 +54,20 @@ export async function updatePost(req, res) {
             return res.status(400).json({ error: 'At least one field (title or content) is required' });
         }
 
-        const post = await Post.findByIdAndUpdate(id, update, { new: true });
+        const post = await Post.findById(id);
 
         if (!post) {
             return res.status(404).json({ error: 'Post not found' });
         }
+
+        if(post.author.toString()!==req.user._id.toString()){
+            return res.status(403).json({ error: "Not authorized" });
+        }
+
+        if (title) post.title = title;
+        if (content) post.content = content;
+
+        await post.save();  
 
         return res.status(200).json(post);
 
@@ -70,7 +79,15 @@ export async function updatePost(req, res) {
 export async function deletePost(req, res) {
     try {
         const { id } = req.params;
-        const post = await Post.findByIdAndDelete(id);
+        const post = await Post.findById(id);
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+        if(post.author.toString()!==req.user._id.toString()){
+            return res.status(403).json({ error: "Not authorized" });
+        }
+        await post.deleteOne();
+        
         return res.status(200).json({ status: 'success' })
     }
     catch (error) {
